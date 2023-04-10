ce4d889909940987fa01bdfc6cbc0fe1f12248ec
Dig-Bick <1239343496@qq.com>
2023-04-10 22:42:51 +0800

    Update PostController.java

--- src/main/java/com/example/forum4/controller/PostController.java
+++ src/main/java/com/example/forum4/controller/PostController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/postManagement")
 public class PostController {
     @Autowired
     private PostService postService;
