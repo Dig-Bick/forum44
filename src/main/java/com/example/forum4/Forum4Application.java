d400b144f907770b2b48a89b79f0ada172371261
Dig-Bick <1239343496@qq.com>
2023-04-14 22:27:13 +0800

    webconfig

--- src/main/java/com/example/forum4/Forum4Application.java
+++ src/main/java/com/example/forum4/Forum4Application.java
@@ -3,6 +3,7 @@ package com.example.forum4;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(value = "com.example.forum4.mapper")
--- src/main/java/com/example/forum4/config/WebSecurityConfig.java
+++ src/main/java/com/example/forum4/config/WebSecurityConfig.java
@@ -16,8 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .csrf().disable() // 禁用 CSRF 防护
             .authorizeRequests()
-               .antMatchers("/api/register", "/api/login", "/admin/register", "/admin/login", "/admin/user-management", "/user/list", "/user/delete/{id}", "/api/posts", "/api/categories", "/api/postManagement/posts", "/api/postManagement/posts/**","/api/categories/all","/api/posts/recommended/**","/api/posts/recommended").permitAll()
-
+              // .antMatchers("/api/register", "/api/login", "/admin/register", "/admin/login", "/admin/user-management", "/user/list", "/user/delete/{id}", "/api/posts", "/api/categories", "/api/postManagement/posts", "/api/postManagement/posts/**","/api/categories/all","/api/posts/recommended/**","/api/posts/recommended").permitAll()
+            .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .httpBasic() // 使用基本的 HTTP 身份验证
